1598d01f57be18f68fea003f6e4df6da84215adf
Kay Erik Jenss <kay.jenss@eigenherd.com>
2020-10-30 11:38:33 +0100

    rewrite sidebar skript

--- docs/rerenderSidebar.py
+++ docs/rerenderSidebar.py
@@ -16,17 +16,15 @@ def appendClassMarkdownForEachPackage(packageName):
 sidebar_file = open('_sidebar.md', 'w')
 listOfFile = os.listdir('.')
 
+sidebar_file.write( f"* [EghLib Home](README.md)\n")
+
 for entry in listOfFile:
-    if os.path.isfile(entry) and ".md" in entry and "_" not in entry:
-        if entry == "README.md":
-            entry = entry.replace(" ", "%20")
-            sidebar_file.write( f"* [EghLib Home]({entry})\n")
-        else:
-            name = entry.split(".md") 
-            entry = entry.replace(" ", "%20")
-            sidebar_file.write(f"* [{name[0]}]({entry})\n")
+    if os.path.isfile(entry) and ".md" in entry and "_" not in entry and entry != "README.md":
+        name = entry.split(".md") 
+        entry = entry.replace(" ", "%20")
+        sidebar_file.write(f"* [{name[0]}]({entry})\n")
     elif os.path.isdir(entry):
-        sidebar_file.write( f"  * [{entry}]({entry}/readme)\n")
+        sidebar_file.write( f"  * [{entry}]({entry}/README.md)\n")
         appendClassMarkdownForEachPackage(entry)
 
 sidebar_file.close()
